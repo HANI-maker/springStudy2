a7c43146d6457b542fa8d9642e03a85dba01f2ea
HANI-maker <84309174+HANI-maker@users.noreply.github.com>
2023-08-20 19:54:44 +0900

    해커톤날 공부 - 게시글작성, 게시글조회(멤버별, 게시글id별), 카테고리 테이블 작성

---
+++ nnbnbn/build/resources/main/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO Member (email,name)
+VALUES ('tom@naver.com', 'tom');
+
+INSERT INTO Member (email,name)
+VALUES ('harry@naver.com', 'harry');
+
+INSERT INTO Member (email,name)
+VALUES ('chul@naver.com', 'chul');
+
+INSERT INTO Member (email,name)
+VALUES ('lion@naver.com', 'lion');
+
+INSERT INTO Member (email,name)
+VALUES ('univ@naver.com', 'univ');
+
+INSERT INTO Category (name)
+VALUES ('Science');
+
+INSERT INTO Category (name)
+VALUES ('Philosophy');
+
+INSERT INTO Category (name)
+VALUES ('Literature');
